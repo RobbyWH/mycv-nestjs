cdcfa95408ae79b45c1f3d99a409cc45f0f86538
robby.widyahartono2 <robby.widyahartono@flash-coffee.com>
2021-08-18 16:26:38 +0700

    decorator + interceptor

---
+++ src/users/interceptors/current-user.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  UseInterceptors,
+  Injectable,
+} from '@nestjs/common';
+import { UsersService } from '../users.service';
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private usersService: UsersService) {}
+
+  async intercept(context: ExecutionContext, handler: CallHandler) {
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.session || {};
+
+    if (userId) {
+      const user = await this.usersService.findOne(userId);
+      request.currentUser = user;
+    }
+
+    // run the route handler
+    return handler.handle();
+  }
+}
