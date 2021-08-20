8c9864cd71ec59ab9ce8d1fd9a1fa69528d6be34
robby.widyahartono2 <robby.widyahartono@flash-coffee.com>
2021-08-20 13:56:26 +0700

    update

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
+// import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+// import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
@@ -14,7 +15,11 @@ import { CurrentUserInterceptor } from './interceptors/current-user.interceptor'
     UsersService,
     AuthService,
     // global interceptor, applied to the all controller
-    { provide: APP_INTERCEPTOR, useClass: CurrentUserInterceptor },
+    // { provide: APP_INTERCEPTOR, useClass: CurrentUserInterceptor },
   ],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
